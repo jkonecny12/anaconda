d794fac9ab595bd3207fffa6b548b81ed0ac8816
Jiri Konecny <jkonecny@redhat.com>
2023-09-05 13:09:03 +0200

    Store webui-desktop in our libexec subfolder
    We should have all our scripts there to make things less confusing.

--- pyanaconda/ui/webui/__init__.py
+++ pyanaconda/ui/webui/__init__.py
@@ -100,7 +100,7 @@ class CockpitUserInterface(ui.UserInterface):
         else:
             profile_name = FIREFOX_THEME_DEFAULT
 
-        proc = startProgram(["/usr/libexec/webui-desktop",
+        proc = startProgram(["/usr/libexec/anaconda/webui-desktop",
                             "-t", profile_name, "-r", str(int(self.remote)),
                             "/cockpit/@localhost/anaconda-webui/index.html"],
                             reset_lang=False)
